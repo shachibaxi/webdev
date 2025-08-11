25e936fa8adb84215f0e7501b3dcba76aebc7901
shachibaxi <shachi2006@gmail.com>
2025-08-11 22:22:34 +0530

    Add calculator folder files and cheat sheet

---
+++ calculator/script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById('display');
+
+function appendValue(val) {
+    display.value += val;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value) || '';
+    } catch {
+        display.value = 'Error';
+    }
+}
+
